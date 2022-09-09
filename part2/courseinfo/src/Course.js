85b5d8bac5d7a6982153240c35fb7e89bdc8d745
ThomasAlexanderMann <97682942+ThomasAlexanderMann@users.noreply.github.com>
2022-09-09 17:31:03 +0100

    Finished exercises 2.1 to 2.5

---
+++ part2/courseinfo/src/Course.js
@@ -0,0 +1,29 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {parts.map((p) => (
+      <Part key={p.id} part={p} />
+    ))}
+  </>
+);
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={course.parts.reduce((s, p) => s + p.exercises, 0)} />
+    </>
+  );
+};
+
+export default Course;
