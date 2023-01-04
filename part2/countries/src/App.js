109c4bbc58051af87e9ff8043f7d998865a354c2
ThomasAlexanderMann <97682942+ThomasAlexanderMann@users.noreply.github.com>
2023-01-04 18:04:59 +0000

    exercise 2.14 Data for countries, step 3

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,38 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+// API KEY
+const api_key = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
+const weatherDefault = {
+  name: "",
+  icon: "",
+  windSpeed: "",
+  temp: "",
+};
+
 export const CountryDetails = ({ country }) => {
+  const [weather, setWeather] = useState(weatherDefault);
+  console.log("weather", weather);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${country.latlng[0]}&lon=${country.latlng[1]}&exclude=minutely,hourly,daily,alerts&appid=${api_key}&units=metric`
+      )
+      .then((response) => {
+        const singleWeatherReport = response.data.list[0];
+        setWeather({
+          name: response.data.city.name,
+          icon: singleWeatherReport?.["weather"]?.[0]?.icon,
+          temp: singleWeatherReport?.main?.temp,
+          windSpeed: singleWeatherReport?.wind?.speed,
+        });
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+  }, []);
+
   return (
     <>
       <h2>{country.name.common}</h2>
@@ -15,14 +46,31 @@ export const CountryDetails = ({ country }) => {
       {Object.keys(country.languages).map((lang, index) => (
         <p key={index}>{lang}</p>
       ))}
-      {console.log("country.flags.png", country.flags.png)}
-      <img src={country.flags.png} alt={`The flag of ${country.name.common}`} />
+      <img
+        style={{ maxWidth: "300px" }}
+        src={country.flags.png}
+        alt={`The flag of ${country.name.common}`}
+      />
+      <h2>Weather in {weather.name}</h2>
+      <p>
+        <strong>temperature: {weather.temp} Celcius</strong>
+      </p>
+      {weather.icon && (
+        <img
+          style={{ maxWidth: "100px" }}
+          src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
+        />
+      )}
+      <p>
+        <strong>wind: {weather.windSpeed} metres/second</strong>
+      </p>
     </>
   );
 };
 
 function App() {
   const [AllCountries, setAllCountries] = useState([]);
+  console.log("api_key", api_key);
 
   console.log("first country in array: ", AllCountries[0]);
 
